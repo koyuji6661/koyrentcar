abffcc911778e6d3531b04185fb1cf105261abc2
devv <dev@ittron.indonesia.com>
2024-07-03 19:35:33 +0700

    3 juni 2024 amstir

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 const App = () => {
 
@@ -17,6 +19,17 @@ const App = () => {
       localStorage.setItem("theme", "light");
     }
   }, [theme]);
+
+// AOS Initialization
+React.useEffect(() => {
+  AOS.init({
+    offset: 100,
+    duration: 800,
+    easing: 'ease-in-sine',
+    delay: 100,
+  });
+  AOS.refresh()
+}, []);
 
   return (
     <div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,18 +8,24 @@ const Hero = ({theme}) => {
     <div className='dark:bg-black dark:text-white duration-300 relative -z-20'>
         <div className="container min-h-[620px] flex">
             <div className='grid place-items-center grid-cols-1 sm:grid-cols-2'>
-                <div className='order-1 sm:order-2'>
+                <div data-aos="zoom-in" data-aos-duration="1500" className='order-1 sm:order-2'>
                     <img src={theme === "dark" ? carPng : yellowCarPng} 
                     className='relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]'
                     />
                 </div>
                 <div className='order-2 sm:order-1 space-y-5 sm:pr-32'>
-                    <p className='text-primary text-2xl font-serif'>Effortless</p>
-                    <h1 className='text-5xl lg:text-7xl font-semibold font-serif'>Car Rental</h1>
-                    <p>
+                    <p data-aos="fade-up" className='text-primary text-2xl font-serif'>Effortless</p>
+                    <h1 data-aos="fade-up" data-aos-delay="600" className='text-5xl lg:text-7xl font-semibold font-serif'>Car Rental</h1>
+                    <p
+                    data-aos="fade-up"
+                    data-aos-delay="1000"
+                    >
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo officiis suscipit aperiam, error corrupti esse qui mollitia exercitationem iusto fugit eius quam temporibus natus. Dolores magnam ab aperiam vero id.
                     </p>
-                    <button className='btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300'>
+                    <button
+                    data-aos="fade-up"
+                    data-aos-duration="1500" 
+                    className='btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80 duration-300'>
                         Get Started
                     </button>
                 </div>
